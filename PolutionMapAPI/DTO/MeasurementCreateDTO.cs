0548ac93fadb6fd2a0e40e72ec5df929887fb882
HolySpiridon <alex1209022@outlook.com>
2021-06-21 18:34:03 +0400

    Add mapping for Measurement to MeasurementReadDTO

--- PolutionMapAPI/DTO/MeasurementCreateDTO.cs
+++ PolutionMapAPI/DTO/MeasurementCreateDTO.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections.Generic;
 using PolutionMapAPI.Models;
 
 namespace PolutionMapAPI.DTO
 {
-    public record MeasurementsCreateDTO (string Type, List<Feature> Features);
-    public record Feature (string Type, MeasurementProperties Properties, Geometry Geometry);
-    public record Geometry (string Type, List<double> Coorginates);
+    public record MeasurementsCreateDTO(List<Feature> Features);
+    public record Feature
+    {
+        public int Noise { get; set; }
+        public int Polution { get; set; }
+        public DateTime Timestamp { get; set; }
+        public Geometry Geometry { get; set; }
+    }
+    public record Geometry (List<double> Coordinates);
 }
